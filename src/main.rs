220dfe40ef9762f6e9030da163d1c1013ee17955
S Smith <root@madeof.glass>
2022-04-19 13:12:02 -0500

    Fresh start
    Signed-off-by: S Smith <root@madeof.glass>

--- src/main.rs
+++ src/main.rs
@@ -32,83 +32,6 @@ fn main() {
 }
 
 fn game_loop() {
-    debug!("loop starting! CPU: {}", screeps::game::cpu::get_used());
-
-    debug!("running spawns");
-    for spawn in screeps::game::spawns::values() {
-        debug!("running spawn {}", spawn.name());
-        let body = [Part::Move, Part::Move, Part::Carry, Part::Work];
-
-        if spawn.energy() >= body.iter().map(|p| p.cost()).sum() {
-            // create a unique name, spawn.
-            let name_base = screeps::game::time();
-            let mut additional = 0;
-            let res = loop {
-                let name = format!("{}-{}", name_base, additional);
-                let res = spawn.spawn_creep(&body, &name);
-
-                if res == ReturnCode::NameExists {
-                    additional += 1;
-                } else {
-                    break res;
-                }
-            };
-
-            if res != ReturnCode::Ok {
-                warn!("couldn't spawn: {:?}", res);
-            }
-        }
-    }
-
-    debug!("running creeps");
-    for creep in screeps::game::creeps::values() {
-        let name = creep.name();
-        debug!("running creep {}", name);
-        if creep.spawning() {
-            continue;
-        }
-
-        if creep.memory().bool("harvesting") {
-            if creep.store_free_capacity(Some(ResourceType::Energy)) == 0 {
-                creep.memory().set("harvesting", false);
-            }
-        } else {
-            if creep.store_used_capacity(None) == 0 {
-                creep.memory().set("harvesting", true);
-            }
-        }
-
-        if creep.memory().bool("harvesting") {
-            let source = &creep
-                .room()
-                .expect("room is not visible to you")
-                .find(find::SOURCES)[0];
-            if creep.pos().is_near_to(source) {
-                let r = creep.harvest(source);
-                if r != ReturnCode::Ok {
-                    warn!("couldn't harvest: {:?}", r);
-                }
-            } else {
-                creep.move_to(source);
-            }
-        } else {
-            if let Some(c) = creep
-                .room()
-                .expect("room is not visible to you")
-                .controller()
-            {
-                let r = creep.upgrade_controller(&c);
-                if r == ReturnCode::NotInRange {
-                    creep.move_to(&c);
-                } else if r != ReturnCode::Ok {
-                    warn!("couldn't upgrade: {:?}", r);
-                }
-            } else {
-                warn!("creep room has no controller!");
-            }
-        }
-    }
-
     let time = screeps::game::time();
 
     if time % 32 == 3 {
@@ -116,7 +39,7 @@ fn game_loop() {
         cleanup_memory().expect("expected Memory.creeps format to be a regular memory object");
     }
 
-    info!("done! cpu: {}", screeps::game::cpu::get_used())
+    debug!("done! cpu: {}", screeps::game::cpu::get_used())
 }
 
 fn cleanup_memory() -> Result<(), Box<dyn std::error::Error>> {
